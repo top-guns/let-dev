6a8b1c5e98ba2d697504659bd7c1e2ce7b57efee
igor-berezhnoy <igor.127@gmail.com>
2024-07-10 23:06:59 +0330

    Add command descriptions

--- commands/config/.edit_config.sh
+++ commands/config/.edit_config.sh
@@ -3,18 +3,13 @@
 
 _edit_config() {
   local FILE_TO_OPEN="$1"
-  local FILE_TITLE="$2"
 
   local FILE_NAME=$(basename $FILE_TO_OPEN)
   local CURRENT_COMMAND=$(basename $0)
 
-  if [ "$1" = "--help" ]; then
-      echo "Open $FILE_TITLE in the preferred text editor"
-      echo "Usage:"
-      echo "  :config:$CURRENT_COMMAND"
-      echo "Examples:"
-      echo "  :config:$CURRENT_COMMAND"
-      return 0
+  if [ "$2" = "--help" ]; then
+      echo "$COMMAND_HELP"
+      return
   fi
 
   # Check if the file exists
@@ -23,5 +18,5 @@ _edit_config() {
     return 1
   fi
 
-  sudo "$LETDEV_HOME/commands/:/edit" "$FILE_TO_OPEN"
+  sudo "$LETDEV_HOME/commands/edit" "$FILE_TO_OPEN"
 }
