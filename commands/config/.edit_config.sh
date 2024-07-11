b454ef37ceb7ce531c1548d766a8b4fd792b2215
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2024-07-11 22:49:07 +0330

    Formatting

--- commands/config/.edit_config.sh
+++ commands/config/.edit_config.sh
@@ -2,21 +2,21 @@
 # set -euo pipefail
 
 _edit_config() {
-  local FILE_TO_OPEN="$1"
+    local FILE_TO_OPEN="$1"
 
-  local FILE_NAME=$(basename $FILE_TO_OPEN)
-  local CURRENT_COMMAND=$(basename $0)
+    local FILE_NAME=$(basename $FILE_TO_OPEN)
+    local CURRENT_COMMAND=$(basename $0)
 
-  if [ "$2" = "--help" ]; then
-      echo "$COMMAND_HELP"
-      return
-  fi
+    if [ "$2" = "--help" ]; then
+        echo "$COMMAND_HELP"
+        return
+    fi
 
-  # Check if the file exists
-  if [ ! -f "$FILE_TO_OPEN" ]; then
-    echo "The $FILE_TO_OPEN file cannot be found"
-    return 1
-  fi
+    # Check if the file exists
+    if [ ! -f "$FILE_TO_OPEN" ]; then
+        echo "The $FILE_TO_OPEN file cannot be found"
+        return 1
+    fi
 
-  sudo "$LETDEV_HOME/commands/edit" "$FILE_TO_OPEN"
+    sudo "$LETDEV_HOME/commands/edit" "$FILE_TO_OPEN"
 }
