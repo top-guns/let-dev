33d9cc459e54a707118b4428dfabbfbc44a057a8
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2024-07-20 15:02:20 +0330

    Fix and improve commands

--- commands/config/.edit_config.sh
+++ commands/config/.edit_config.sh
@@ -3,19 +3,25 @@
 
 _edit_config() {
     local FILE_TO_OPEN="$1"
+    local CAN_BE_CREATED="${2:-false}"
 
     local FILE_NAME=$(basename $FILE_TO_OPEN)
     local CURRENT_COMMAND=$(basename $0)
 
-    if [ "$2" = "--help" ]; then
+    if [ "$2" = "--help" ] || [ "$2" = "help" ]; then
         echo "$COMMAND_HELP"
         return
     fi
 
     # Check if the file exists
     if [ ! -f "$FILE_TO_OPEN" ]; then
-        echo "The $FILE_TO_OPEN file cannot be found"
-        return 1
+        if [ "$CAN_BE_CREATED" = "true" ]; then
+            echo "The $FILE_TO_OPEN file does not exist. Creating it..."
+            touch "$FILE_TO_OPEN"
+        else
+            echo "File $FILE_TO_OPEN is not found"
+            return 1
+        fi
     fi
 
     sudo "$LETDEV_HOME/commands/edit" "$FILE_TO_OPEN"
