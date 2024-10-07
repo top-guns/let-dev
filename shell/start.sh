baab294e489b736711c367fc95cb499ee536d5b5
igor-berezhnoy <107257800+igor-berezhnoy@users.noreply.github.com>
2024-10-07 14:21:59 +0300

    Rename system commands folder

--- shell/start.sh
+++ shell/start.sh
@@ -3,12 +3,14 @@
 # Load aliases to make them available in the current shell
 shopt -s expand_aliases
 # source $HOME/.bashrc
+# Load letdev commands
+source $LETDEV_HOME/create-aliases
 
-source $LETDEV_PATH/shell/lib/settings.sh
-source $LETDEV_PATH/shell/lib/util.sh
-source $LETDEV_PATH/shell/lib/commands.sh
-source $LETDEV_PATH/shell/lib/history.sh
-source $LETDEV_PATH/shell/lib/display.sh
+source $LETDEV_HOME/shell/lib/settings.sh
+source $LETDEV_HOME/shell/lib/utils.sh
+source $LETDEV_HOME/shell/lib/commands.sh
+source $LETDEV_HOME/shell/lib/history.sh
+source $LETDEV_HOME/shell/lib/display.sh
 
 SHELL_MODE=false
 PINNED_COMMANDS=()
@@ -53,6 +55,90 @@ handle_winch() {
     redraw_screen
     # Reinstall the signal handler
     trap handle_winch SIGWINCH
+}
+
+input=()
+
+# Function to read multiline user input (read while the user doesn't press Ctrl+D)
+read_command() {
+    input=()
+    input_line_no=0
+
+    # Initial cursor position
+    # Get the total number of lines
+    total_lines=$(tput lines)
+    # Subtract 1 because line numbers start at 0
+    row=$((total_lines - 1))
+    # Initial column position
+    col=0
+
+    # Move cursor to initial position
+    tput cup $row $col
+
+    # Disable echo
+    stty -echo
+
+    # Read one character at a time
+    while IFS= read -r -n1 key; do
+        # Check for arrow keys
+        case $key in
+            $'\e') # If key is escape sequence
+                read -r -n2 key # read two more chars
+                case $key in
+                    '[A') # Up arrow
+                        ((row--))
+                        ;;
+                    '[B') # Down arrow
+                        ((row++))
+                        ;;
+                    '[C') # Right arrow
+                        ((col++))
+                        ;;
+                    '[D') # Left arrow
+                        ((col--))
+                        ;;
+                esac
+                ;;
+            *)
+                # Add the key to the current line
+                input[$row]="${input[$row]:0:$col}$key${input[$row]:$col}"
+                echo input
+                read
+                ((col++))
+                ;;
+        esac
+
+        # Display the current state of the command input
+        display_input_text
+
+        # Move cursor to new position
+        tput cup $row $col
+    done
+
+    # Enable echo
+    stty echo
+}
+
+display_input_text() {
+    # Calculate the start row to display the input text at the bottom
+    start_row=$(( $(tput lines) - ${#input[@]} ))
+
+    # Clear the lines from start_row to the bottom
+    for (( i=$start_row; i<$(tput lines); i++ )); do
+        tput cup $i 0
+        tput el
+    done
+
+    # Print the input text
+    for (( i=0; i<${#input[@]}; i++ )); do
+        tput cup $(( $start_row + $i )) 0
+        echo "${input[$i]}"
+    done
+}
+
+display_command_input() {
+    tput cup $(( $(tput lines) - 1 )) 0
+    read_command
 }
 
 # Function to wait for the next command
